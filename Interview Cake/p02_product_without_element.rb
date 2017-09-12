f0f3127e015c329979627c63981300bfb1f02ae9
Syrie Bianco <syriebianco@gmail.com>
2017-09-12 08:43:24 -0700

    Rename method

--- Interview Cake/p02_product_without_element.rb
+++ Interview Cake/p02_product_without_element.rb
@@ -1,4 +1,4 @@
-def product_with_current_element(arr)
+def product_without_current_element(arr)
   arr.each_index.map { |i| (arr[0...i] + arr[i + 1..-1]).reduce(:*) }
 end
 
@@ -8,9 +8,9 @@ arr1 = [1, 2, 3, 4]
 arr2 = [5, 6, 7, 8]
 arr3 = [0, 4, 2, 1]
 
-result1 = product_with_current_element(arr1)
-result2 = product_with_current_element(arr2)
-result3 = product_with_current_element(arr3)
+result1 = product_without_current_element(arr1)
+result2 = product_without_current_element(arr2)
+result3 = product_without_current_element(arr3)
 
 expect1 = [ 2 * 3 * 4, 1 * 3 * 4, 1 * 2 * 4, 1 * 2 * 3 ]
 expect2 = [ 6 * 7 * 8, 5 * 7 * 8, 5 * 6 * 8, 5 * 6 * 7 ]
