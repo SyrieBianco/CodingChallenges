2428d5a166df984f7ae340dcb173b732ab525ebb
Syrie Bianco <syriebianco@gmail.com>
2017-09-15 08:56:32 -0700

    Get it working

--- Codewars/pickPeaks.js
+++ Codewars/pickPeaks.js
@@ -6,15 +6,21 @@ function pickPeaks(arr){
     let prev, curr, next;
     [prev, curr, next] = [arr[i - 1], arr[i], arr[i + 1]];
 
-    // first check to see if it is greater than the element before it
+    // if the current element is greater than the previous element..
     if ( curr > prev ) {
+      // we are not in a plateau
+      plateau.true = false;
       // if it is equal to the next element...
       if ( curr === next) {
+        // we are in a plateau
         // update the plateau object
         plateau.pos = i;
         plateau.peak = curr;
         plateau.true = true;
+      // if it current element is *also* greater than the next element...
       } else if (curr > next) {
+        // we are at a relative peak
+        // update the tracker object
         tracker.pos.push(i);
         tracker.peaks.push(curr);
       }
@@ -22,10 +28,26 @@ function pickPeaks(arr){
 
     // if the value goes down and we were in a plateau
     if (curr > next && plateau.true) {
-      tracker.pos.push(i);
-      tracker.peaks.push(curr);
+      // that means the plateau has ended
+      plateau.true = false;
+
+      // and that it represented a relative peak
+      // => update the tracker object
+      tracker.pos.push(plateau.pos);
+      tracker.peaks.push(plateau.peak);
     }
   }
 
   return tracker;
+}
+
+function updatePlateau(plateau, i, curr){
+  plateau.pos = i;
+  plateau.peak = curr;
+  plateau.true = true;
+}
+
+function updateTracker(tracker, pos, peak){
+  tracker.pos.push(pos);
+  tracker.peak.push(peak);
 }
