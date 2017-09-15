9585e0d0e750ca0d6d15ccc160cf8b6090af259b
Syrie Bianco <syriebianco@gmail.com>
2017-09-15 10:35:43 -0700

    Complete first pass of rowSumOddNumbers

--- Codewars/rowSumOddNumbers.js
+++ Codewars/rowSumOddNumbers.js
@@ -1,16 +1,81 @@
 function rowSumOddNumbers(i) {
   let startNum = 0;
-  for (i; i > 0; i--) {
+  for (let j = i; j > 0; j--) {
     startNum += 2 * 1;
-    if (i === 1) startNum -= 1;
+    if (j === 1) startNum -= 1;
   }
 
   return startNum + (i - 1) * 2;
 }
 
-console.log(rowSumOddNumbers(1) === 1);
-console.log(rowSumOddNumbers(42) == 74088);
+console.log("Test 1");
+console.log("Input: 1");
+let result1 = rowSumOddNumbers(1);
+let expect1 = 1;
+let test1 = result1 === expect1;
+test1 ? console.log("passed") : console.log("failed");
+console.log("expected:", expect1);
+console.log("   result:", result1);
+
+console.log("Test 2");
+console.log("Input: 2");
+let result2 = rowSumOddNumbers(2);
+let expect2 = 8;
+let test2 = result2 === expect2;
+test2 ? console.log("passed") : console.log("failed");
+console.log("expected:", expect2);
+console.log("   result:", result2);
+
+console.log("Test 3");
+console.log("Input: 3");
+let result3 = rowSumOddNumbers(3);
+let expect3 = 27;
+let test3 = result3 === expect3;
+test3 ? console.log("passed") : console.log("failed");
+console.log("expected:", expect3);
+console.log("   result:", result3);
+
+console.log("Test 4");
+console.log("Input: 4");
+let result4 = rowSumOddNumbers(4);
+let expect4 = 64;
+let test4 = result4 === expect4;
+test4 ? console.log("passed") : console.log("failed");
+console.log("expected:", expect4);
+console.log("   result:", result4);
+
+console.log("Test 5");
+console.log("Input: 5");
+let result5 = rowSumOddNumbers(5);
+let expect5 = 125;
+let test5 = result5 === expect5;
+test5 ? console.log("passed") : console.log("failed");
+console.log("expected:", expect5);
+console.log("   result:", result5);
+
+console.log("Test 6");
+console.log("Input: 6");
+let result6 = rowSumOddNumbers(6);
+let expect6 = 216;
+let test6 = result6 === expect6;
+test6 ? console.log("passed") : console.log("failed");
+console.log("expected:", expect6);
+console.log("   result:", result6);
+
+console.log("Test 7");
+console.log("Input: 7");
+let result7 = rowSumOddNumbers(42);
+let expect7 = 74088;
+let test7 = result7 === expect7;
+test7 ? console.log("passed") : console.log("failed");
+console.log("expected:", expect7);
+console.log("   result:", result7);
 
 
-// running concerns:
-  // will my for loop mutate my input "i", and does it matter?
+//                    1                   =>   1
+//                 3     5                =>   8
+//              7     9    11             =>   27
+//          13    15    17    19          =>   64
+//       21    23    25    27    29       =>   125
+//    31    33    35    37    39    41    =>   216
+//
---
+++ Codewars/rowSumOddNumbers.rb
@@ -0,0 +1,76 @@
+def row_sum_odd_numbers(i)
+  elements_before = (0...i).reduce(:+)
+  start_num = (elements_before * 2 - 1) + 2
+  (start_num * i) + (0...i).reduce(:+) * 2
+end
+
+puts "Test 1"
+puts "Input: 1"
+result1 = row_sum_odd_numbers(1)
+expect1 = 1
+test1 = result1 === expect1
+puts test1 ? "~*~ PASSED ~*~" : "~*~ FAILED ~*~"
+puts "expected: #{expect1}"
+puts "  result: #{result1}"
+
+puts "Test 2"
+puts "Input: 2"
+result2 = row_sum_odd_numbers(2)
+expect2 = 8
+test2 = result2 === expect2
+puts test2 ? "~*~ PASSED ~*~" : "~*~ FAILED ~*~"
+puts "expected: #{expect2}"
+puts "  result: #{result2}"
+
+puts "Test 3"
+puts "Input: 3"
+result3 = row_sum_odd_numbers(3)
+expect3 = 27
+test3 = result3 === expect3
+puts test3 ? "~*~ PASSED ~*~" : "~*~ FAILED ~*~"
+puts "expected: #{expect3}"
+puts "  result: #{result3}"
+
+puts "Test 4"
+puts "Input: 4"
+result4 = row_sum_odd_numbers(4)
+expect4 = 64
+test4 = result4 === expect4
+puts test4 ? "~*~ PASSED ~*~" : "~*~ FAILED ~*~"
+puts "expected: #{expect4}"
+puts "  result: #{result4}"
+
+puts "Test 5"
+puts "Input: 5"
+result5 = row_sum_odd_numbers(5)
+expect5 = 125
+test5 = result5 === expect5
+puts test5 ? "~*~ PASSED ~*~" : "~*~ FAILED ~*~"
+puts "expected: #{expect5}"
+puts "  result: #{result5}"
+
+puts "Test 6"
+puts "Input: 6"
+result6 = row_sum_odd_numbers(6)
+expect6 = 216
+test6 = result6 === expect6
+puts test6 ? "~*~ PASSED ~*~" : "~*~ FAILED ~*~"
+puts "expected: #{expect6}"
+puts "  result: #{result6}"
+
+puts "Test 7"
+puts "Input: 7"
+result7 = row_sum_odd_numbers(42)
+expect7 = 74088
+test7 = result7 === expect7
+puts test7 ? "~*~ PASSED ~*~" : "~*~ FAILED ~*~"
+puts "expected: #{expect7}"
+puts "  result: #{result7}"
+
+
+puts "                    1                   =>   1"
+puts "                 3     5                =>   8"
+puts "              7     9    11             =>   27"
+puts "          13    15    17    19          =>   64"
+puts "       21    23    25    27    29       =>   125"
+puts "    31    33    35    37    39    41    =>   216"
