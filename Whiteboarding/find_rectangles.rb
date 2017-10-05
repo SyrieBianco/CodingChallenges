679356dfb5913056498a7869259882f2f165260d
Syrie Bianco <syriebianco@gmail.com>
2017-10-04 19:06:21 -0700

    Refactor find_rectangles.rb

--- Whiteboarding/find_rectangles.rb
+++ Whiteboarding/find_rectangles.rb
@@ -1,6 +1,13 @@
-# Imagine we have an image. We’ll represent this image as a simple 2D array where every pixel is a 1 or a 0.
+# Imagine we have an image. We’ll represent this image as a simple
+# 2D array where every pixel is a 1 or a 0.
 
-# The image you get is known to have N rectangles of 0s on a background of 1s. Write a function that takes in the image and outputs the coordinates of all the 0 rectangles -- top-left and bottom-right; or top-left, width and height.
+# The image you get is known to have N rectangles of 0s on a background
+# of 1s.
+
+# Write a function that takes in the image and outputs the coordinates
+# of all the 0 rectangles --
+# op-left and bottom-right;
+# or top-left, width and height.
 
 
 def find_image(arr)
@@ -13,9 +20,7 @@ def find_image(arr)
       if el == 0
 
         next if arr[row_i][col_i - 1] == 0
-        if row_i != 0
-          next if arr[row_i - 1][col_i] == 0
-        end
+        next if row_i != 0 && arr[row_i - 1][col_i] == 0
 
         top_lefts << [row_i, col_i]
       end
@@ -26,7 +31,7 @@ def find_image(arr)
 
   top_lefts.each do |top_left|
     width, height = 0, 0
-    width_i, height_i = top_left[1], top_left[0]
+    height_i, width_i = top_left
 
     until arr[top_left[0]][width_i] != 0
       width += 1
@@ -48,22 +53,22 @@ end
 
 # input:
 image = [
-#  0  1  2  3  4  5  6
-  [1, 1, 1, 1, 1, 1, 1], # 0
+  # 0  1  2  3  4  5  6
+  [1, 1, 0, 1, 1, 1, 1], # 0
   [1, 1, 1, 1, 1, 1, 1], # 1
   [1, 1, 1, 0, 0, 0, 1], # 2
   [1, 0, 1, 0, 0, 0, 1], # 3
   [1, 0, 1, 1, 1, 1, 1], # 4
   [1, 0, 1, 0, 0, 1, 1], # 5
   [1, 1, 1, 0, 0, 1, 1], # 6
-  [1, 1, 1, 1, 1, 1, 1], # 7
-];
+  [1, 1, 1, 1, 1, 0, 0], # 7
+        ]
 
 
 
 output = { [2, 3] => [3, 2], [3, 1] => [1, 3], [5, 3] => [2, 2] }
-# { [coordinate] => [w,h] }
+# [[coordinate], [w,h]]
 
 
-  puts "test 1"
-  p find_image(image) 
+puts "test 1"
+p find_image(image)
