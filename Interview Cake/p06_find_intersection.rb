10f070793f91bbb101f44aa49510eafe0415ad30
Syrie Bianco <syriebianco@gmail.com>
2017-10-05 23:21:36 -0700

    Begin to build out test cases for p06

--- Interview Cake/p06_find_intersection.rb
+++ Interview Cake/p06_find_intersection.rb
@@ -34,7 +34,7 @@ def generate_results(coord_1, coord_2, length_1, length_2)
 end
 
 # ^
-# |   rect 1
+# |   rect a
 # 5   .______________.
 # |   |              |
 # 4   |              |
@@ -43,40 +43,57 @@ end
 # |   |     |        |          |
 # 2   ._____|________.          |
 # |         |                   |
-# 1         .___________________.   rect 2
+# 1         .___________________.   rect b
 # |
 # 0 -- 1 -- 2 -- 3 -- 4 -- 5 -- >
 
+rect_a = { 'x' => 1, 'y' => 2, 'w' => 3, 'h' => 3 }
 
-# determine relative position of rect_1 to rect_2
-# relative positions = { 'x' => rect_1[x] <=> rect_2[x], 'y' => rect_1[y] <=> rect_2[y] }
-# possible positions:
-#  'x':
-#      0: left edges of rectangles are coterminus
-#         new_x = rect_1[x]
-#         new_w = [rect_1[w], rect_2[w]].min
-#      1: rect_1 is to the left of rect_2
-#         new_x = rect_2[x]
-#         new_w = rect1[w] - (rect_2[x] - rect_1[x])
-#     -1: rect_2 is to the left of rect_1
-#         new_x = rect_1[x]
-#         new_w = rect_2[w] - (rect_1[x] - rect_2[x])
-#  'y':
-#      0: bottom edges of rectangles are coterminus
-#         new_y = rect_1[y]
-#         new_h = [rect_1[h], rect_2[h]].min
-#      1: rect_1 is to the left of rect_2
-#         new_y = rect_2[y]
-#         new_h = rect1[h] - (rect_2[y] - rect_1[y])
-#     -1: rect_2 is to the left of rect_1
-#         new_y = rect_1[y]
-#         new_h = rect_2[h] - (rect_1[y] - rect_2[y])
+rect_b = { 'x' => 3, 'y' => 7, 'w' => 4, 'h' => 3 }
 
-# use relative positions to determine verticies of rectangle of intersection
+# ^
+# |   rect c
+# 5   .______________________________.
+# |   |                              |
+# 4   |                              |
+# |   |                              |
+# 3   |     .___________________.    |
+# |   |     |                   |    |
+# 2   ._____|___________________|____.
+# |         |                   |
+# 1         .___________________.   rect d
+# |
+# 0 -- 1 -- 2 -- 3 -- 4 -- 5 -- >
 
+rect_a = { 'x' => 1, 'y' => 2, 'w' => 3, 'h' => 3 }
 
-rect_1 = { 'x' => 2, 'y' => 3, 'w' => 3, 'h' => 3 }
+rect_b = { 'x' => 3, 'y' => 7, 'w' => 4, 'h' => 3 }
 
-rect_2 = { 'x' => 3, 'y' => 7, 'w' => 4, 'h' => 3 }
+# ^
+# |   rect e
+# 5   ._____.
+# |   |     |
+# 4   |     |
+# |   |     |
+# 3   ._____|_____________.
+# |   |     |             |
+# 2   |     |             |
+# |   |     |             |
+# 1   ._____._____________.   rect f
+# |
+# 0 -- 1 -- 2 -- 3 -- 4 -- 5 -- >
+
+rect_a = { 'x' => 1, 'y' => 2, 'w' => 3, 'h' => 3 }
+
+rect_b = { 'x' => 3, 'y' => 7, 'w' => 4, 'h' => 3 }
 
 result_1 = { 'x' => 3, 'y' => 3, 'w' => 2, 'h' => 2 }
+
+
+
+      rect1 = {
+        'left_x'   => 0,
+        'bottom_y' => 0,
+        'width'    => 10,
+        'height'   => 5
+      }
