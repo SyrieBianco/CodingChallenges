9662ebc317de5c578d1cbcb9a04a83eb626c3c49
Syrie Bianco <syriebianco@gmail.com>
2017-10-05 23:48:58 -0700

    Refactor p06, perfect test cases

--- Interview Cake/lib/p06_find_intersection.rb
+++ Interview Cake/lib/p06_find_intersection.rb
@@ -2,8 +2,8 @@
 
 def find_intersection(rect_1, rect_2)
 
-  new_x = new_dims(rect_1[x], rect_2[x], rect_1[w], rect_2[w])
-  new_y = new_dims(rect_1[y], rect_2[y], rect_1[h], rect_2[h])
+  new_x = new_dims(rect_1['x'], rect_2['x'], rect_1['w'], rect_2['w'])
+  new_y = new_dims(rect_1['y'], rect_2['y'], rect_1['h'], rect_2['h'])
 
  { 'x' => new_x['coord'], 'y' => new_y['coord'],
    'w' => new_x['length'], 'h' => new_y['length'] }
@@ -18,9 +18,9 @@ def new_dims(coord_1, coord_2, length_1, length_2)
     result['coord'] = coord_1
     result['length'] = [length_1, length_2].min
   when 1
-    result = generate_results(coord_1, coord_2, length_1, length_2)
-  when -1
     result = generate_results(coord_2, coord_1, length_2, length_1)
+  when -1
+    result = generate_results(coord_1, coord_2, length_1, length_2)
   end
   result
 end
@@ -50,11 +50,11 @@ end
 rect_a = { 'x' => 1, 'y' => 2, 'w' => 3, 'h' => 3 }
 rect_b = { 'x' => 2, 'y' => 1, 'w' => 4, 'h' => 2 }
 
-result_1 = find_intersection(rect_a, rect_b)
-result_2 = find_intersection(rect_b, rect_a)
+result1 = find_intersection(rect_a, rect_b)
+result2 = find_intersection(rect_b, rect_a)
 
-expect_1 = { 'x' => 2, 'y' => 2, 'w' => 2, 'h' => 1 }
-expect_2 = expect_1
+expect1 = { 'x' => 2, 'y' => 2, 'w' => 2, 'h' => 1 }
+expect2 = expect1
 
 # ^
 # 6         .___________________.
@@ -72,12 +72,14 @@ expect_2 = expect_1
 # 0 -- 1 -- 2 -- 3 -- 4 -- 5 -- 6 -- 7 -- 8 ->
 
 rect_c = { 'x' => 1, 'y' => 2, 'w' => 6, 'h' => 3 }
+rect_d = { 'x' => 2, 'y' => 1, 'w' => 4, 'h' => 5 }
 
-result_3 = find_intersection(rect_b, rect_c)
-result_4 = find_intersection(rect_d, rect_c)
 
-expect_3 = { 'x' => 2, 'y' => 2, 'w' => 4, 'h' => 1 }
-expect_4 = { 'x' => 2, 'y' => 2, 'w' => 4, 'h' => 3 }
+result3 = find_intersection(rect_b, rect_c)
+result4 = find_intersection(rect_d, rect_c)
+
+expect3 = { 'x' => 2, 'y' => 2, 'w' => 4, 'h' => 1 }
+expect4 = { 'x' => 2, 'y' => 2, 'w' => 4, 'h' => 3 }
 
 # ^
 # |   rect e
@@ -94,13 +96,13 @@ expect_4 = { 'x' => 2, 'y' => 2, 'w' => 4, 'h' => 3 }
 # 0 -- 1 -- 2 -- 3 -- 4 -- 5 -- >
 
 rect_e = { 'x' => 1, 'y' => 1, 'w' => 1, 'h' => 4 }
-rect_f = { 'x' => 1, 'y' => 1, 'w' => 4, 'h' => 3 }
+rect_f = { 'x' => 1, 'y' => 1, 'w' => 4, 'h' => 2 }
 
-result_5 = find_intersection(rect_e, rect_f)
-result_6 = find_intersection(rect_e, rect_e)
+result5 = find_intersection(rect_e, rect_f)
+result6 = find_intersection(rect_e, rect_e)
 
-expect_5 = { 'x' => 1, 'y' => 1, 'w' => 1, 'h' => 2 }
-expect_6 = rect_e
+expect5 = { 'x' => 1, 'y' => 1, 'w' => 1, 'h' => 2 }
+expect6 = rect_e
 
 # not my test cases:
 rect1 = { 'x' => 0, 'y' => 0, 'w' => 10, 'h' => 5 }
@@ -108,11 +110,11 @@ rect2 = { 'x' => 8, 'y' => 4, 'w' => 6, 'h' => 3 }
 rect3 = { 'x' => 2, 'y' => 1, 'w' => 6, 'h' => 3 }
 
 
-result_7 = find_intersection(rect1, rect2)
-result_8 = find_intersection(rect1, rect3)
+result7 = find_intersection(rect1, rect2)
+result8 = find_intersection(rect1, rect3)
 
-expect_7 = { 'x' => 8, 'y' => 4, 'w' => 2, 'h' => 1 }
-expect_8 = rect3
+expect7 = { 'x' => 8, 'y' => 4, 'w' => 2, 'h' => 1 }
+expect8 = rect3
 
 p ' ~~ test 1 ~~ '
 p " result = #{result1}"
